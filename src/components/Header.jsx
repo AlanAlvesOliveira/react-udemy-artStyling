8a105495dba238153074e62e4bd97eb2249199ad
Alan Alves <alan.oliveira@teledatabrasil.com.br>
2025-06-09 12:24:18 -0300

    example styled-components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,24 @@
 import logo from '../assets/logo.png';
+import { styled } from "styled-components";
+
+const TituloComponentStyled = styled.h1` //gera um novoo component
+  font-size: 1.5rem;
+  font-weight: 600;
+  letter-spacing: 0.4em;
+  text-align: center;
+  text-transform: uppercase;
+  color: #9a3412;
+  font-family: 'Pacifico', cursive;
+  margin: 0;
+
+`
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
+      <TituloComponentStyled>ReactArt</TituloComponentStyled>
+      {/* <h1>ReactArt</h1> */}
       <p>A community of artists and art-lovers.</p>
     </header>
   );
